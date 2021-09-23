7ebab6efa277dc67464dcc6c28b5b44c2b707fe0
umairJanjua <56170485+UmairJanjua6@users.noreply.github.com>
2021-09-23 11:30:51 +0500

    code re-structured

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,16 +3,8 @@
  */
 const VLand = artifacts.require ('VLAND');
 
-/**
- * Contract version 2 contract artifact 
- */
-const VLand1 = artifacts.require ('VLAND1');
-
 // For deploying proxy contract ie: V1
 const {deployProxy} = require ('@openzeppelin/truffle-upgrades');
-
-// for upgrading proxy contract ie: V2
-// const {upgradeProxy} = require ('@openzeppelin/truffle-upgrades');
 
 module.exports = async function (deployer) {
   /**
@@ -20,14 +12,4 @@ module.exports = async function (deployer) {
    */
   const instance = await deployProxy (VLand, {deployer});
   console.log ('instance: ', instance.address);
-
-  /**
-   * upgrading V1 to V2
-   * 0x5b6b8841bde6e15f74b331219b080e85081c479b ---- Address of TransparentUpgradeableProxy
-   */
-  //   const upgraded = await upgradeProxy (
-  //   '0x5b6b8841bde6e15f74b331219b080e85081c479b',
-  //   VLand1,
-  //   {deployer}
-  // );
 };
